8a6726da58fe3c0c7c4332a99bb1af69bdb715cb
yuk-tanaka <paradisecreek70@gmail.com>
2023-09-01 07:40:38 +0900

    最低限の体裁を整えた

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <svelte:head>
-  <title>日本酒ゴーアラウンド金沢 公式サイト</title>
+  <title>日本酒ゴーアラウンド2023金沢 公式サイト</title>
 </svelte:head>
 
 <main>
@@ -16,35 +16,43 @@
     <Row>
       <Column padding>
         <h1>
+          <!--TODO style設定の外だし-->
           <ImageLoader
             alt="メインビジュアル"
+            style="max-height: 993px; max-width: 702px;"
             src="mainVisual.jpg"
           />
         </h1>
       </Column>
     </Row>
   </Grid>
-
   <Grid>
     <Row padding>
       <Column>
         <h2>日本酒ゴーアラウンド金沢、開催決定！</h2>
-
-      </Column>
-    </Row>
-    <Row padding>
-      <Column>
-        <h2>参加方法</h2>
         <p>
-          説明説明説明
+          毎年10月1日は<strong>日本酒の日</strong>。<br>
+          この日を祝うべく全国各地で開催されるはしご酒イベント <strong><a href="https://nga-osaka.com/" target="_blank">日本酒ゴーアラウンド</a></strong>が、2023年より金沢で開催されることとなりました。
+        </p>
+        <p>
+          詳しい開催情報や参加店舗については、このページで随時情報提供していきますので、お楽しみに！
         </p>
       </Column>
     </Row>
+<!--    <Row padding>-->
+<!--      <Column>-->
+<!--        <h2>参加方法</h2>-->
+<!--        <p>-->
+<!--          説明説明説明-->
+<!--        </p>-->
+<!--      </Column>-->
+<!--    </Row>-->
     <Row padding>
       <Column>
         <h2>参加店舗・地図</h2>
+        <!--TODO responsive-->
         <iframe
-          src="https://www.google.com/maps/d/u/0/embed?mid=1U0tKEJ0QEQvCCn23VLKqJ87QjAwXhRw&ehbc=2E312F" width="400" height="600"
+          src="https://www.google.com/maps/d/u/0/embed?mid=1U0tKEJ0QEQvCCn23VLKqJ87QjAwXhRw&ehbc=2E312F" width="360" height="600"
           title="マップ"
         />
       </Column>
@@ -52,7 +60,3 @@
   </Grid>
 
 </main>
-
-<style>
-
-</style>
